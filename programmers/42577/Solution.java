41e01c6deb6aaf5cb5811c72e2b40d07004cc8f8
parkjbdev <parkjbdev@gmail.com>
2020-07-12 17:45:25 +0900

    42577 Init

---
+++ programmers/42577/Solution.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+class Solution {
+	public static void main(String[] args)
+	{
+		String[] phone_book = { "119", "97674223", "1195524421" };
+		System.out.println(solution(phone_book));
+	}
+
+	public static boolean solution(String[] phone_book) 
+	{
+		HashMap<String, Integer> map = new HashMap<>();
+		
+
+
+		return true;
+    }
+}
