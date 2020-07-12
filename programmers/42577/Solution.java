7aed77369823379d4c857d542839c89982958426
parkjbdev <parkjbdev@gmail.com>
2020-07-12 23:06:19 +0900

    Use HashMap properly

--- programmers/42577/Solution.java
+++ programmers/42577/Solution.java
@@ -1,15 +1,34 @@
+import java.util.HashMap;
+
 class Solution {
 	public static void main(String[] args)
 	{
-		String[] phone_book = { "119", "97674223", "1195524421" };
+		String[] phone_book = {"97674223", "1195524421", "1195" };
 		System.out.println(solution(phone_book));
 	}
 
 	public static boolean solution(String[] phone_book) 
 	{
-		for (int i = 0; i < phone_book.length - 1; i++)
-			for(int j = i + 1;j < phone_book.length;j++)
-				if(phone_book[i].startsWith(phone_book[j]) || phone_book[j].startsWith(phone_book[i]))	return false;
+		HashMap<String, Boolean> map = new HashMap<>();
+		for(String phone : phone_book)
+		{
+			for (int i = 1; i <= phone.length(); i++)
+			{
+				if (map.get(phone.substring(0, i)) != null)
+				{
+					if (map.get(phone.substring(0, i)))	return false;
+					else
+					{
+						if (phone.substring(0, i).equals(phone))	return false;
+					}
+				}
+				else
+				{
+					map.put(phone.substring(0, i), false);
+					if (i == phone.length())	map.put(phone, true);
+				}
+			}
+		}
 
 		return true;
 	}
