fe0ade82437b5332368c0ba914eee2b6c8972d55
parkjbdev <parkjbdev@gmail.com>
2020-07-12 21:39:44 +0900

    Add Solution 42577

--- programmers/42577/Solution.java
+++ programmers/42577/Solution.java
@@ -1,3 +1,4 @@
+import java.util.Map;
 import java.util.HashMap;
 class Solution {
 	public static void main(String[] args)
@@ -9,8 +10,24 @@ class Solution {
 	public static boolean solution(String[] phone_book) 
 	{
 		HashMap<String, Integer> map = new HashMap<>();
-		
 
+		for (String phone : phone_book)
+		{
+			for (Map.Entry<String, Integer> entry : map.entrySet())
+			{
+				int length = phone.length() > entry.getKey().length() ? entry.getKey().length() : phone.length();
+
+				if (length == entry.getKey().length())
+				{
+					if(phone.substring(0, length).equals(entry.getKey()))	return false;
+				}
+				else
+				{
+					if (entry.getKey().substring(0, length).equals(phone))	return false;
+				}
+			}
+			map.put(phone, map.getOrDefault(phone, 0) + 1);
+		}
 
 		return true;
     }
