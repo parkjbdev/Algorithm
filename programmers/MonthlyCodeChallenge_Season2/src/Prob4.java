82876c1cbf9301e7ef467088d66b6e764036f898
parkjbdev <parkjbdev@gmail.com>
2021-04-21 14:49:24 +0900

    Programmers Monthly Code Challenge Prob4 - Initialize Problem

--- programmers/MonthlyCodeChallenge_Season2/src/Prob4.java
+++ programmers/MonthlyCodeChallenge_Season2/src/Prob4.java
@@ -1,10 +1,33 @@
-//class Solution {
-//	public long[] solution(int n, int z, int[][] roads, long[] queries) {
-//		long[] answer = {};
-//		return answer;
-//	}
-//}
+class Prob4 {
+	private final int[][] adjacentMatrix;
+	private final long[] queries;
+	public Prob4(int n, int z, int[][] roads, long[] queries)
+	{
+		adjacentMatrix = new int[n][n];
+		for (int i = 0; i < n; i++) adjacentMatrix[i][i] = z;
+		for (int[] road : roads) adjacentMatrix[road[0]][road[1]] = road[2];
+		this.queries = queries;
+	}
 
-public class Prob4 {
+	public long[] solve()
+	{
+		long[] answer = new long[queries.length];
+
+		for (int i = 0;i < queries.length;i++)
+		{
+			long targetCost = queries[i];
+			answer[i] = query(targetCost);
+		}
+
+		return answer;
+	}
+
+	private long query(final long targetCost)
+	{
+		long cost = 0;
+		long cnt = 0;
+
+		return cnt;
+	}
 
 }
