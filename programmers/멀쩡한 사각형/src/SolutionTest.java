8ca738b3f534fd2eefc9065171bfe0a8dc49a1b2
parkjbdev <parkjbdev@gmail.com>
2021-06-28 02:49:33 +0900

    Add TestCase - Programmers 62048

---
+++ programmers/멀쩡한 사각형/src/SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolutionTest {
+  @Test
+  void testcase() {
+    assertEquals(80, new Solution().solution(8, 12));
+  }
+}
