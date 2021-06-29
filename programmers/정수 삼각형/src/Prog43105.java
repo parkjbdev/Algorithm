4c3ed2f84d0dccbe27b8262f65d8ee662a75df84
parkjbdev <parkjbdev@gmail.com>
2021-06-29 16:09:11 +0900

    Refactor Programmers 43105 for better performance (Remove Flattening)

--- programmers/정수 삼각형/src/Prog43105.java
+++ programmers/정수 삼각형/src/Prog43105.java
@@ -1,33 +1,21 @@
-import java.util.Arrays;
-
 public class Prog43105 {
-  public final int[] triangle;
-  private int[] sum;
+  public final int[][] triangle;
+  private final int[][] sum;
 
   public Prog43105(int[][] triangle) {
-    this.triangle = Arrays.stream(triangle).flatMapToInt(Arrays::stream).toArray();
-    this.sum = new int[this.triangle.length];
+    this.triangle = triangle;
+    this.sum = new int[triangle.length][];
+    for (int i = 0; i < triangle.length; i++) this.sum[i] = new int[triangle[i].length];
   }
 
   public int solve() {
-    return maxSum(0);
+    return maxSum(0, 0);
   }
 
-  private int maxSum(int idx) {
-    if (idx >= triangle.length) return 0;
-    if (sum[idx] == 0) sum[idx] = triangle[idx] + Math.max(maxSum(leftIdx(idx)), maxSum(rightIdx(idx)));
-    return sum[idx];
-  }
-
-  private int getFloor(int idx) {
-    return (int) ((1 + Math.sqrt(1 + 8 * idx)) / 2);
-  }
-
-  private int leftIdx(int idx) {
-    return getFloor(idx) + idx;
-  }
-
-  private int rightIdx(int idx) {
-    return getFloor(idx) + idx + 1;
+  private int maxSum(int idx1, int idx2) {
+    if (idx1 >= triangle.length || idx2 >= triangle[idx1].length) return 0;
+    if (sum[idx1][idx2] == 0)
+      sum[idx1][idx2] = triangle[idx1][idx2] + Math.max(maxSum(idx1 + 1, idx2), maxSum(idx1 + 1, idx2 + 1));
+    return sum[idx1][idx2];
   }
 }
