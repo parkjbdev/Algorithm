3ee5e0cb864ce4d4242e1983ac9ef6bbdd38cf1b
parkjbdev <parkjbdev@gmail.com>
2021-07-09 14:50:47 +0900

    Solve Programmers 64601 - 크레인 인형뽑기 게임

--- programmers/크레인 인형뽑기 게임/src/Prob64061Test.java
+++ programmers/크레인 인형뽑기 게임/src/Prob64061Test.java
@@ -1,4 +1,12 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class Prob64061Test {
-  
+  @Test
+  void case1() {
+    assertEquals(4,
+      new Prob64061(new int[][]{{0, 0, 0, 0, 0}, {0, 0, 1, 0, 3}, {0, 2, 5, 0, 1}, {4, 2, 4, 4, 2}, {3, 5, 1, 3, 1}},
+        new int[]{1, 5, 3, 5, 1, 2, 1, 4}).solve());
+  }
 }
