7be02c48f2c00e668d763b24b1d99bf40678f2b3
parkjbdev <dev@parkjb.com>
2024-10-06 00:06:28 +0900

    refactor: dijkstra

--- Essentials/dijkstra.py
+++ Essentials/dijkstra.py
@@ -2,37 +2,38 @@
 # https://www.acmicpc.net/problem/1753
 
 from math import inf
-from heapq import heappush, heappop
+from heapq import heappush as push, heappop as pop
 
 # 입력
 V, E = map(int, input().split())
-K = int(input()) - 1
+K = int(input()) - 1 # 시작 정점 번호
+EDGES = [tuple(map(int, input().split())) for _ in range(E)]
 
 # Adjacent List
-adj = [[] for _ in range(V)]
-for _ in range(E):
-    x, y, curr_w = map(int, input().split())
-    adj[x - 1].append((curr_w, y - 1))
+ADJ_EDGES = [[] for _ in range(V)]
+
+for edge in EDGES:
+    ADJ_EDGES[edge[0] - 1].append((edge[2], edge[1] - 1))
 
 # Distance
 distance = [inf for _ in range(V)]
 distance[K] = 0
 
 # Priority Heap: 거리순으로 정렬
-heap = []
-heappush(heap, (0, K))
+pq = []
+push(pq, (0, K))
 
-while heap:
+while pq:
     # 노드 방문
-    curr_w, curr_v = heappop(heap)
-    if distance[curr_v] < curr_w:
+    cost, vertex = pop(pq)
+    if distance[vertex] < cost:
         continue
 
     # 가장 작은 weight 가진 곳으로..
-    for next_w, next_v in adj[curr_v]:
-        new_distance = distance[curr_v] + next_w
-        if new_distance < distance[next_v]:
-            distance[next_v] = new_distance
-            heappush(heap, (new_distance, next_v))
+    for next_c, next_v in ADJ_EDGES[vertex]:
+        cmp_distance = distance[vertex] + next_c
+        if cmp_distance < distance[next_v]:
+            distance[next_v] = cmp_distance
+            push(pq, (cmp_distance, next_v))
 
 print(*map(lambda x: str(x).upper(), distance), sep="\n")
